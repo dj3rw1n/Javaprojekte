e0db2ef3f5e1df3ec1fabdc4917e2775851fab24
Erwin <3rw1ngaming@gmail.com>
2020-01-21 17:35:20 +0100

    fertih

--- Tanne.java
+++ Tanne.java
@@ -16,6 +16,15 @@ public class Tanne
         s1 = new GLKegel (pX, 150, pZ,150,150);
         s1.setzeTextur ("grün.jpg");
         s1.drehe (90,0,0);
+        
+        s2 = new GLKegel (pX, 250, pZ,150,150);
+        s2.setzeTextur ("grün.jpg");
+        s2.drehe (90,0,0);
+        
+        s3 = new GLKegel (pX, 350, pZ,150,150);
+        s3.setzeTextur ("grün.jpg");
+        s3.drehe (90,0,0);
+        
     }
 
     
